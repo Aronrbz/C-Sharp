7c6031bebc0443a4889b1160b7e71796b202f0b2
Aron Bustos <ar.bustos@duocuc.cl>
2024-07-26 22:25:49 -0400

    se suben primeros codigos en C#

--- 01 - Basico/01 - Instalaciones/clase1.cs
+++ 01 - Basico/01 - Instalaciones/clase1.cs
@@ -12,3 +12,52 @@ namespace MiNombre
 }
 
 
+using System;
+
+namespace HolaPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, soy un programa en C#");
+        }
+    }
+}
+
+
+
+using System;
+
+namespace TresMensajes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola");
+            Console.WriteLine("estoy aprendiendo");
+            Console.WriteLine("programacion en C#");
+        }
+    }
+}
+
+
+using System;
+
+namespace Presentacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nombre: Aron");
+            Console.WriteLine("Edad: 35");
+            Console.WriteLine("Hobby: Música");
+        }
+    }
+}
+
+
+
+
